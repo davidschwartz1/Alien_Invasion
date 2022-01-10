261098a010d790e1dd9c3948170090fd48975547
davidschwartz1 <dvds314@gmail.com>
2022-01-10 23:46:39 +0200

    inital commit

---
+++ alien_ invasion.py
@@ -0,0 +1,22 @@
+import pygame
+from settings import Settings
+from ship import Ship
+import game_functions as gf
+
+def run_game():
+    pygame.init()
+    ai_settings = Settings()
+    screen = pygame.display.set_mode(
+        (ai_settings.screen_width, ai_settings.screen_height))
+    pygame.display.set_caption("Alien Invasion")
+    # Make a ship.
+    ship = Ship(ai_settings, screen)
+
+
+    while True:
+        gf.check_events(ship)
+        ship.update()
+        gf.update_screen(ai_settings, screen, ship)
+
+run_game()
+
